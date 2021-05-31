1861d0481965d3da85437914c36a5d93f4a9cd35
DokuroGitHub <dovt58@gmail.com>
2021-05-31 14:33:43 +0700

    best item_layout so far

--- FeedBackApplication/app/src/main/java/com/example/feedbackapplication/Adapter/EnrollmentAdapter.java
+++ FeedBackApplication/app/src/main/java/com/example/feedbackapplication/Adapter/EnrollmentAdapter.java
@@ -9,9 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.feedbackapplication.R;
-import com.example.feedbackapplication.model.Class;
 import com.example.feedbackapplication.model.Enrollment;
-import com.example.feedbackapplication.model.Trainee;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -23,11 +21,9 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.List;
 
 public class EnrollmentAdapter extends FirebaseRecyclerAdapter<Enrollment, EnrollmentAdapter.MyViewHolder> {
+    private final ClickListener clickListener;
     public List<String> classNameList;
     public List<String> traineeNameList;
-    private ClickListener clickListener;
-    private List<com.example.feedbackapplication.model.Class> classList;
-    private List<com.example.feedbackapplication.model.Trainee> traineeList;
 
     /**
      * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
@@ -40,71 +36,6 @@ public class EnrollmentAdapter extends FirebaseRecyclerAdapter<Enrollment, Enrol
         this.clickListener = clickListener;
     }
 
-    public void fetchData() {
-        FirebaseDatabase.getInstance().getReference("Class").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                classNameList = null;
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    Class temp = dataSnapshot.getValue(Class.class);
-                    if (temp != null) {
-                        //classList.add(temp);
-                        //classNameList.add(temp.getClassName());
-                        String x = temp.getClassName();
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
-        FirebaseDatabase.getInstance().getReference("Trainee").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                traineeList = null;
-                int i = 0;
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    if (dataSnapshot != null) {
-                        Trainee temp = new Trainee();
-                        temp.setUserName(dataSnapshot.child("UserName").getValue(String.class));
-                        temp.setName(dataSnapshot.child("Name").getValue(String.class));
-                        //traineeNameList.add(temp.getName());
-                        //traineeList.add(i++,temp);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
-    }
-
-    public String fetchClassName(int id) {
-        String className = "no_info";
-        if (classList != null) {
-            for (com.example.feedbackapplication.model.Class class_desu : classList) {
-                if (class_desu.getClassID() == id) {
-                    return class_desu.getClassName();
-                }
-            }
-        }
-        return className;
-    }
-
-    public String fetchTraineeName(String id) {
-        String traineeName = "no_info";
-        if (traineeList != null) {
-            for (Trainee trainee : traineeList) {
-                if (trainee.getUserName().equals(id)) {
-                    return trainee.getName();
-                }
-            }
-        }
-        return traineeName;
-    }
-
     @Override
     protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Enrollment model) {
         FirebaseDatabase.getInstance().getReference("Trainee")
@@ -114,11 +45,8 @@ public class EnrollmentAdapter extends FirebaseRecyclerAdapter<Enrollment, Enrol
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                             if (dataSnapshot != null) {
-                                String temp = dataSnapshot.child("Name").getValue(String.class);
-                                if (temp != null) {
-                                    temp = "Trainee Name: " + temp;
-                                    holder.txtTraineeName.setText(temp);
-                                }
+                                String temp = String.valueOf(dataSnapshot.child("Name").getValue(String.class));
+                                holder.txtTraineeName.setText(temp);
                             }
                         }
                     }
@@ -134,11 +62,8 @@ public class EnrollmentAdapter extends FirebaseRecyclerAdapter<Enrollment, Enrol
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                             if (dataSnapshot != null) {
-                                String temp = dataSnapshot.child("className").getValue(String.class);
-                                if (temp != null) {
-                                    temp = "Class Name: " + temp;
-                                    holder.txtClassName.setText(temp);
-                                }
+                                String temp = String.valueOf(dataSnapshot.child("className").getValue(String.class));
+                                holder.txtClassName.setText(temp);
                             }
                         }
                     }
@@ -147,9 +72,8 @@ public class EnrollmentAdapter extends FirebaseRecyclerAdapter<Enrollment, Enrol
                     public void onCancelled(@NonNull DatabaseError error) {
                     }
                 });
-        holder.txtTraineeID.setText("Trainee ID: " + model.getTraineeID());
-        holder.txtClassID.setText("Class ID: " + model.getClassID());
-
+        holder.txtTraineeID.setText(String.valueOf(model.getTraineeID()));
+        holder.txtClassID.setText(String.valueOf(model.getClassID()));
         holder.btnView.setOnClickListener(v -> clickListener.viewClicked(model));
         holder.btnEdit.setOnClickListener(v -> clickListener.updateClicked(model));
         holder.btnDelete.setOnClickListener(v -> clickListener.deleteClicked(model));
@@ -172,7 +96,7 @@ public class EnrollmentAdapter extends FirebaseRecyclerAdapter<Enrollment, Enrol
         void deleteClicked(Enrollment enrollment);
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView txtClassID, txtClassName, txtTraineeID, txtTraineeName;
         //ImageView edit;
         FloatingActionButton btnView;
